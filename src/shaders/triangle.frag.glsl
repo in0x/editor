695a8eb27ab7450c381a01fc91c35f6f27068b30
Phil Welsch <pwelsch86@gmail.com>
2022-12-04 20:39:30 +0100

    rougly gamma correct output fragments

--- src/shaders/triangle.frag.glsl
+++ src/shaders/triangle.frag.glsl
@@ -6,5 +6,7 @@ layout(location = 0) in vec3 color;
 void main()
 {
 	// outputColor = vec4(1.0, 0.64, 0.0, 1.0);
-	outputColor = vec4(color, 1.0);
+	float gamma = 2.2;
+    vec3 correctedColor = pow(color, vec3(1.0/gamma));
+	outputColor = vec4(correctedColor, 1.0);
 }

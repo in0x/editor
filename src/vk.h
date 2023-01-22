1cc69f34f0ae7f04cfd5841d3c41c87c43ab866c
Phil Welsch <pwelsch86@gmail.com>
2023-01-22 17:10:45 +0100

    move volk into a third-party folder

--- src/vk.h
+++ src/vk.h
@@ -4,44 +4,44 @@
 #define VK_NO_PROTOTYPES
 #include <MoltenVK/mvk_vulkan.h>
 
-#include "volk/volk.h"
+#include "3p/volk/volk.h"
 
 constexpr u32 C_TARGET_VK_VERSION = VK_API_VERSION_1_2;
 
-#define VK_CHECK(op)                                                                      \
-    do                                                                                    \
-    {                                                                                     \
-        VkResult result = op;                                                             \
-        switch (result)                                                                   \
-        {                                                                                 \
-        case VK_SUCCESS:                                                                  \
-        case VK_TIMEOUT:                                                                  \
-        case VK_NOT_READY:                                                                \
-        case VK_SUBOPTIMAL_KHR:                                                           \
-        case VK_ERROR_OUT_OF_DATE_KHR:                                                    \
-				break;                                                                    \
-        case VK_ERROR_OUT_OF_HOST_MEMORY:                                                 \
-            ASSERT_MSG(result == VK_SUCCESS, "Error OUT_OF_HOST_MEMORY (%d)", result);    \
-            break;                                                                        \
-        case VK_ERROR_OUT_OF_DEVICE_MEMORY:                                               \
-            ASSERT_MSG(result == VK_SUCCESS, "Error OUT_OF_DEVICE_MEMORY (%d)", result);  \
-            break;                                                                        \
-        case VK_ERROR_INITIALIZATION_FAILED:                                              \
-            ASSERT_MSG(result == VK_SUCCESS, "Error INITIALIZATION_FAILED (%d)", result); \
-            break;                                                                        \
-        case VK_ERROR_LAYER_NOT_PRESENT:                                                  \
-            ASSERT_MSG(result == VK_SUCCESS, "Error LAYER_NOT_PRESENT (%d)", result);     \
-            break;                                                                        \
-        case VK_ERROR_EXTENSION_NOT_PRESENT:                                              \
-            ASSERT_MSG(result == VK_SUCCESS, "Error EXTENSION_NOT_PRESENT (%d)", result); \
-            break;                                                                        \
-        case VK_ERROR_INCOMPATIBLE_DRIVER:                                                \
-            ASSERT_MSG(result == VK_SUCCESS, "Error INCOMPATIBLE_DRIVER (%d)", result);   \
-            break;                                                                        \
-        default:                                                                          \
-            ASSERT_MSG(result == VK_SUCCESS, "Error Code %d", result);                    \
-            break;                                                                        \
-        }                                                                                 \
+#define VK_CHECK(op)                                                                                    \
+    do                                                                                                  \
+    {                                                                                                   \
+        VkResult __vkcr_result = (op);                                                                  \
+        switch (__vkcr_result)                                                                          \
+        {                                                                                               \
+        case VK_SUCCESS:                                                                                \
+        case VK_TIMEOUT:                                                                                \
+        case VK_NOT_READY:                                                                              \
+        case VK_SUBOPTIMAL_KHR:                                                                         \
+        case VK_ERROR_OUT_OF_DATE_KHR:                                                                  \
+				break;                                                                                  \
+        case VK_ERROR_OUT_OF_HOST_MEMORY:                                                               \
+            ASSERT_MSG(__vkcr_result == VK_SUCCESS, "Error OUT_OF_HOST_MEMORY (%d)", __vkcr_result);    \
+            break;                                                                                      \
+        case VK_ERROR_OUT_OF_DEVICE_MEMORY:                                                             \
+            ASSERT_MSG(__vkcr_result == VK_SUCCESS, "Error OUT_OF_DEVICE_MEMORY (%d)", __vkcr_result);  \
+            break;                                                                                      \
+        case VK_ERROR_INITIALIZATION_FAILED:                                                            \
+            ASSERT_MSG(__vkcr_result == VK_SUCCESS, "Error INITIALIZATION_FAILED (%d)", __vkcr_result); \
+            break;                                                                                      \
+        case VK_ERROR_LAYER_NOT_PRESENT:                                                                \
+            ASSERT_MSG(__vkcr_result == VK_SUCCESS, "Error LAYER_NOT_PRESENT (%d)", __vkcr_result);     \
+            break;                                                                                      \
+        case VK_ERROR_EXTENSION_NOT_PRESENT:                                                            \
+            ASSERT_MSG(__vkcr_result == VK_SUCCESS, "Error EXTENSION_NOT_PRESENT (%d)", __vkcr_result); \
+            break;                                                                                      \
+        case VK_ERROR_INCOMPATIBLE_DRIVER:                                                              \
+            ASSERT_MSG(__vkcr_result == VK_SUCCESS, "Error INCOMPATIBLE_DRIVER (%d)", __vkcr_result);   \
+            break;                                                                                      \
+        default:                                                                                        \
+            ASSERT_MSG(__vkcr_result == VK_SUCCESS, "Error Code %d", __vkcr_result);                    \
+            break;                                                                                      \
+        }                                                                                               \
     } while (false)
 
 #define VK_ASSERT_VALID(handle) ASSERT(handle != VK_NULL_HANDLE)
